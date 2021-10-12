81bc9700a085241453e3c53dcd28c497286d5e72
KamWit7 <kamilw77@interia.pl>
2021-10-12 22:16:03 +0200

    demo aniamtion :candy:

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './sass/main.scss';
+
+const qs = s => document.querySelector(s);
+const qsa = s => document.querySelectorAll(s);
+const l = s => console.log(s);
+
+const cardItems = qsa('.card__item');
+
+l(cardItems);
+
+const addAniamtion = function (cardItem) {
+  cardItem.classList.add('card__item-animation');
+};
+
+function ani() {
+  [...cardItems].forEach(card => {
+    card.addEventListener('click', () => {
+      addAniamtion(card);
+    });
+  });
+}
+
+ani();
