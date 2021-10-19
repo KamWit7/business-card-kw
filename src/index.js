e0e18515eb4979c4e5f0f52e6b5a425da787ba91
KamWit7 <kamilw77@interia.pl>
2021-10-19 18:49:52 +0200

    Coming Mobile

--- src/index.js
+++ src/index.js
@@ -5,35 +5,22 @@ const qsa = s => document.querySelectorAll(s);
 const l = s => console.log(s);
 
 const cardItems = qsa('.card__item');
+const innerCard = qs('.flip-card-inner');
 
-l(cardItems);
+const cards = [...cardItems];
+const [firstCard, secondCard, thirdCard, fourthCard] = cards;
 
 const addAniamtion = function (cardItem, className) {
   cardItem.classList.add(className);
 };
-const removeAniamtion = function (cardItem, className) {
-  cardItem.classList.remove(className);
-};
 
-function moveCard() {
-  [cardItems[0], cardItems[1], cardItems[2]].forEach((card, idx) => {
-    card.addEventListener('click', () => {
-      addAniamtion(card, 'card__item-animation');
-      idx === 0 ? card.classList.remove('shake') : 0;
-    });
+[firstCard, secondCard, thirdCard].forEach((card, idx) => {
+  card.addEventListener('click', () => {
+    addAniamtion(card, 'card__item-animation');
+    idx === 0 ? card.classList.remove('shake') : 0;
   });
+});
 
-  const innerCard = qs('.flip-card-inner');
-  const backCard = qs('.flip-card-back');
-
-  cardItems[3].addEventListener('click', () => {
-    innerCard.classList.add('flip');
-  });
-
-  // backCard.addEventListener('click', () => {
-  //   cardItems[3].removeEventListener();
-  //   innerCard.classList.toggle('flip');
-  // });
-}
-
-moveCard();
+fourthCard.addEventListener('click', () => {
+  addAniamtion(innerCard, 'flip');
+});
